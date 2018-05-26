4879583e100158ad7ab7b2ef3d8527958c6b5cd9
Steven Hoffman <shoffman@valenciamgmt.com>
2018-05-26 14:54:52 -0700

    ready event
    Signed-off-by: Steven Hoffman <shoffman@valenciamgmt.com>

---
+++ baseball_discord/events/ready.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module BaseballDiscord
+  module Events
+    module MemberJoin
+      extend Discordrb::EventContainer
+
+      ready do |_event|
+      end
+    end
+  end
+end
