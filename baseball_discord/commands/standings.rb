04e7eefd925f494fb1de7acee10d3148c1a633fa
Steven Hoffman <git@fustrate.com>
2018-05-25 19:04:58 -0700

    let’s try this pattern
    just for kicks
    
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- baseball_discord/commands/standings.rb
+++ baseball_discord/commands/standings.rb
@@ -5,90 +5,96 @@ module BaseballDiscord
     module Standings
       extend Discordrb::Commands::CommandContainer
 
-      STATS_STANDINGS = \
-        'https://statsapi.mlb.com/api/v1/standings/regularSeason?' \
-        'leagueId=103,104&season=%<year>d&t=%<t>d&date=%<date>s'
-
-      DIVISIONS = {
-        200 => %w[alw alwest],
-        201 => %w[ale aleast],
-        202 => %w[alc alcentral],
-        203 => %w[nlw nlwest],
-        204 => %w[nle nleast],
-        205 => %w[nlc nlcentral]
-      }.freeze
-
       command(
         :standings,
         min_args: 1,
         description: 'Displays the standings for a division',
         usage: 'standings [division]'
       ) do |event, *args|
-        standings(event, *args)
+        StandingsCommand.run(event, *args)
       end
 
-      def standings(_event, *args)
-        division_id, date = parse_standings_args(args)
+      class StandingsCommand
+        STATS_STANDINGS = \
+          'https://statsapi.mlb.com/api/v1/standings/regularSeason?' \
+          'leagueId=103,104&season=%<year>d&t=%<t>d&date=%<date>s'
 
-        return react_to_event(event, '❓') unless division_id
+        DIVISIONS = {
+          200 => %w[alw alwest],
+          201 => %w[ale aleast],
+          202 => %w[alc alcentral],
+          203 => %w[nlw nlwest],
+          204 => %w[nle nleast],
+          205 => %w[nlc nlcentral]
+        }.freeze
 
-        rows = load_data_from_stats_api(STATS_STANDINGS, date: date)
-          .dig('records')
-          .find { |record| record.dig('division', 'id') == division_id }
-          .dig('teamRecords')
-          .sort_by { |team| team['divisionRank'] }
-          .map { |team| team_standings_data(team) }
-
-        standings_table(rows)
-      end
-
-      protected
-
-      # This should be expanded upon to allow for more date formats
-      def parse_standings_args(args)
-        input = args.join('').downcase
-
-        if input =~ /\A([a-z]+)(\d{4})\z/
-          division_id = find_division(Regexp.last_match[1])
-          date = DateTime.civil(Regexp.last_match[2].to_i, 12, 1)
-        else
-          division_id = find_division(input)
-          date = Time.now
+        def self.run(*args)
+          new.run(*args)
         end
 
-        [division_id, date]
-      end
+        def run(_event, *args)
+          division_id, date = parse_standings_args(args)
 
-      def team_standings_data(team)
-        r_diff_sign = team['runDifferential'].negative? ? '' : '+'
+          return react_to_event(event, '❓') unless division_id
 
-        [
-          team.dig('team', 'name'),
-          team['wins'],
-          team['losses'],
-          team['gamesBack'],
-          team.dig('leagueRecord', 'pct'),
-          "#{r_diff_sign}#{team['runDifferential']}",
-          team.dig('streak', 'streakCode')
-        ]
-      end
+          rows = load_data_from_stats_api(STATS_STANDINGS, date: date)
+            .dig('records')
+            .find { |record| record.dig('division', 'id') == division_id }
+            .dig('teamRecords')
+            .sort_by { |team| team['divisionRank'] }
+            .map { |team| team_standings_data(team) }
 
-      def standings_table(rows)
-        table = Terminal::Table.new(
-          rows: rows,
-          headings: %w[Team W L GB % rDiff STRK]
-        )
+          standings_table(rows)
+        end
 
-        table.align_column(1, :right)
-        table.align_column(2, :right)
-        table.align_column(3, :right)
-        table.align_column(5, :right)
+        protected
 
-        "```\n#{table}\n```"
-      end
+        # This should be expanded upon to allow for more date formats
+        def parse_standings_args(args)
+          input = args.join('').downcase
 
-      def find_division(input)
-        DIVISIONS.find { |_, value| value.include?(input) }&.first
+          if input =~ /\A([a-z]+)(\d{4})\z/
+            division_id = find_division(Regexp.last_match[1])
+            date = DateTime.civil(Regexp.last_match[2].to_i, 12, 1)
+          else
+            division_id = find_division(input)
+            date = Time.now
+          end
+
+          [division_id, date]
+        end
+
+        def team_standings_data(team)
+          r_diff_sign = team['runDifferential'].negative? ? '' : '+'
+
+          [
+            team.dig('team', 'name'),
+            team['wins'],
+            team['losses'],
+            team['gamesBack'],
+            team.dig('leagueRecord', 'pct'),
+            "#{r_diff_sign}#{team['runDifferential']}",
+            team.dig('streak', 'streakCode')
+          ]
+        end
+
+        def standings_table(rows)
+          table = Terminal::Table.new(
+            rows: rows,
+            headings: %w[Team W L GB % rDiff STRK]
+          )
+
+          table.align_column(1, :right)
+          table.align_column(2, :right)
+          table.align_column(3, :right)
+          table.align_column(5, :right)
+
+          "```\n#{table}\n```"
+        end
+
+        def find_division(input)
+          DIVISIONS.find { |_, value| value.include?(input) }&.first
+        end
       end
     end
   end
