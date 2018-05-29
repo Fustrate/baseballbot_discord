5807b8df83822e7b3abd63999e51098da150e89f
Steven Hoffman <shoffman@valenciamgmt.com>
2018-05-29 10:07:13 -0700

    analogboy deleted the old role…
    Signed-off-by: Steven Hoffman <shoffman@valenciamgmt.com>

--- baseball_discord/bot.rb
+++ baseball_discord/bot.rb
@@ -47,7 +47,7 @@ module BaseballDiscord
 
     # The 'Verified' role on each of the above servers.
     VERIFIED_ROLES = {
-      400_516_567_735_074_817 => 449_686_034_507_366_440, # baseball
+      400_516_567_735_074_817 => 451_065_609_556_590_602, # baseball
       450_792_745_553_100_801 => 450_811_043_997_024_258 # test
     }.freeze
 
