1d4a259cf383c79fcc9fbbe564491fcfb62bcb68
Steven Hoffman <git@fustrate.com>
2018-05-29 18:57:04 -0700

    try to add abbrs to nicknames
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- baseball_discord/commands/team_roles.rb
+++ baseball_discord/commands/team_roles.rb
@@ -28,36 +28,36 @@ module BaseballDiscord
         PM
 
         TEAM_ROLES = {
-          108 => 448_515_259_561_541_632, # Los Angeles Angels
-          109 => 448_515_130_632_831_005, # Arizona Diamondbacks
-          110 => 448_515_245_812_613_121, # Baltimore Orioles
-          111 => 448_515_246_949_138_443, # Boston Red Sox
-          112 => 448_515_249_268_588_554, # Chicago Cubs
-          113 => 448_515_251_483_443_200, # Cincinnati Reds
-          114 => 448_515_252_615_905_322, # Cleveland Indians
-          115 => 448_515_253_366_685_707, # Colorado Rockies
-          116 => 448_515_254_771_646_475, # Detroit Tigers
-          117 => 448_515_255_686_135_819, # Houston Astros
-          118 => 448_515_257_019_793_418, # Kansas City Royals
-          119 => 448_515_263_965_691_906, # Los Angeles Dodgers
-          120 => 448_517_695_948_980_224, # Washington Nationals
-          121 => 448_515_313_546_559_488, # New York Mets
-          133 => 448_515_319_007_281_162, # Oakland Athletics
-          134 => 448_515_386_522_992_670, # Pittsburgh Pirates
-          135 => 448_517_669_948_227_594, # San Diego Padres
-          136 => 448_517_678_693_351_434, # Seattle Mariners
-          137 => 448_517_675_270_930_442, # San Francisco Giants
-          138 => 448_517_681_818_239_006, # St. Louis Cardinals
-          139 => 448_517_685_047_853_086, # Tampa Bay Rays
-          140 => 448_517_687_962_763_265, # Texas Rangers
-          141 => 448_517_693_470_146_561, # Toronto Blue Jays
-          142 => 448_515_311_314_927_626, # Minnesota Twins
-          143 => 448_515_321_473_662_976, # Philadelphia Phillies
-          144 => 448_515_244_143_280_138, # Atlanta Braves
-          145 => 448_515_250_422_153_226, # Chicago White Sox
-          146 => 448_515_265_882_226_688, # Miami Marlins
-          147 => 448_515_316_360_937_472, # New York Yankees
-          158 => 448_515_267_384_049_674  # Milwaukee Brewers
+          108 => ['LAA', 448_515_259_561_541_632], # Los Angeles Angels
+          109 => ['ARI', 448_515_130_632_831_005], # Arizona Diamondbacks
+          110 => ['BAL', 448_515_245_812_613_121], # Baltimore Orioles
+          111 => ['BOS', 448_515_246_949_138_443], # Boston Red Sox
+          112 => ['CHC', 448_515_249_268_588_554], # Chicago Cubs
+          113 => ['CIN', 448_515_251_483_443_200], # Cincinnati Reds
+          114 => ['CLE', 448_515_252_615_905_322], # Cleveland Indians
+          115 => ['COL', 448_515_253_366_685_707], # Colorado Rockies
+          116 => ['DET', 448_515_254_771_646_475], # Detroit Tigers
+          117 => ['HOU', 448_515_255_686_135_819], # Houston Astros
+          118 => ['KC',  448_515_257_019_793_418], # Kansas City Royals
+          119 => ['LAD', 448_515_263_965_691_906], # Los Angeles Dodgers
+          120 => ['WAS', 448_517_695_948_980_224], # Washington Nationals
+          121 => ['NYM', 448_515_313_546_559_488], # New York Mets
+          133 => ['OAK', 448_515_319_007_281_162], # Oakland Athletics
+          134 => ['PIT', 448_515_386_522_992_670], # Pittsburgh Pirates
+          135 => ['SD',  448_517_669_948_227_594], # San Diego Padres
+          136 => ['SEA', 448_517_678_693_351_434], # Seattle Mariners
+          137 => ['SF',  448_517_675_270_930_442], # San Francisco Giants
+          138 => ['STL', 448_517_681_818_239_006], # St. Louis Cardinals
+          139 => ['TB',  448_517_685_047_853_086], # Tampa Bay Rays
+          140 => ['TEX', 448_517_687_962_763_265], # Texas Rangers
+          141 => ['TOR', 448_517_693_470_146_561], # Toronto Blue Jays
+          142 => ['MIN', 448_515_311_314_927_626], # Minnesota Twins
+          143 => ['PHI', 448_515_321_473_662_976], # Philadelphia Phillies
+          144 => ['ATL', 448_515_244_143_280_138], # Atlanta Braves
+          145 => ['CWS', 448_515_250_422_153_226], # Chicago White Sox
+          146 => ['MIA', 448_515_265_882_226_688], # Miami Marlins
+          147 => ['NYY', 448_515_316_360_937_472], # New York Yankees
+          158 => ['MIL', 448_515_267_384_049_674]  # Milwaukee Brewers
         }.freeze
 
         def update_member_role
@@ -100,7 +100,7 @@ module BaseballDiscord
 
           return react_to_message('❓') unless team_ids.any?
 
-          role_ids = team_ids.map { |team_id| TEAM_ROLES[team_id] }
+          role_ids = team_ids.map { |team_id| TEAM_ROLES.dig(team_id, 1) }
 
           add = @baseball.roles.select { |role| role_ids.include?(role.id) }
 
@@ -108,6 +108,20 @@ module BaseballDiscord
           @member.modify_roles add, all_team_roles_on_server
 
           react_to_message '✅'
+
+          update_nickname(team_ids)
+        end
+
+        def update_nickname(team_ids)
+          abbrs = team_ids.map { |team_id| TEAM_ROLES.dig(team_id, 0) }
+            .compact
+            .map { |abbr| "[#{abbr}]" }
+
+          base_name = @member.nick.gsub(/ \[.*\]\z/, '')
+
+          @member.nick = "#{base_name} #{abbrs}"
+        rescue Discordrb::Errors::NoPermission
+          @bot.logger.info "Couldn't update name for #{@member.distinct}"
         end
 
         def all_team_roles_on_server
