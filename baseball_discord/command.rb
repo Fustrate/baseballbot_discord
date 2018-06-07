e8f280ef08b5433747018b0cc131d200eabff6a5
Steven Hoffman <git@fustrate.com>
2018-06-06 21:43:23 -0700

    fix for tables with titles
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- baseball_discord/command.rb
+++ baseball_discord/command.rb
@@ -74,16 +74,20 @@ module BaseballDiscord
     end
 
     def prettify_table(table)
-      lines = table.to_s.lines.map(&:strip)
-      top_border = lines.shift
-      bottom_border = lines.pop
+      top_border, *middle, bottom_border = table.to_s.lines.map(&:strip)
 
-      new_table = lines.map do |line|
+      new_table = middle.map do |line|
         line[0] == '+' ? "├#{line[1...-1].tr('-+', '─┼')}┤" : line.tr('|', '│')
       end
 
       new_table.unshift "┌#{top_border[1...-1].tr('-+', '─┬')}┐"
       new_table.push "└#{bottom_border[1...-1].tr('-+', '─┴')}┘"
+
+      # Move the T-shaped corners down two rows if there's a title
+      if table.title
+        new_table[0] = new_table[0].tr('┬', '─')
+        new_table[2] = new_table[0].tr('┼', '┬')
+      end
 
       new_table.join("\n")
     end
