2673dd62f9f2a54715c63f96f2b86947ae8dc023
Steven Hoffman <git@fustrate.com>
2019-04-05 12:42:39 -0700

    clean up code, add social stuff
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- game_chat_bot/concerns/color_feed.rb
+++ game_chat_bot/concerns/color_feed.rb
@@ -25,14 +25,27 @@ module GameChatBot
     def send_color_feed_embed_for(item)
       case item['group']
       when 'statcastGFX'
-        send_message <<~DESCRIPTION
-          #{item.dig('data', 'details', 'description_tracking')}
-
-          #{item.dig('data', 'url')}
-        DESCRIPTION
+        send_statcast_message(item)
+      when 'social'
+        send_social_message(item)
       when 'video'
         send_message item.dig('data', 'url', 0, '_')
       end
+    end
+
+    def send_statcast_message(item)
+      send_message <<~DESCRIPTION
+        #{item.dig('data', 'details', 'description_tracking')}
+
+        #{item.dig('data', 'url')}
+      DESCRIPTION
+    end
+
+    def send_social_message(item)
+      tweet = item.dig('data', 'tweet')
+      username = tweet.dig('user', 'screen_name')
+
+      send_message "https://twitter.com/#{username}/status/#{tweet['id']}"
     end
   end
 end
