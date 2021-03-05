a47e56eb3f23bf6418dc763b15af42f5f68ec3b3
Steven Hoffman <git@fustrate.com>
2021-03-04 17:44:36 -0800

    now start fixing the other bot
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- baseball_discord/bot.rb
+++ baseball_discord/bot.rb
@@ -32,7 +32,7 @@ module BaseballDiscord
     # ID of the user allowed to administrate the bot
     ADMIN_ID = 429364871121993728
 
-    INTENTS = %i[server_members direct_messages direct_message_reactions].freeze
+    INTENTS = %i[servers server_members direct_messages direct_message_reactions].freeze
 
     def initialize(attributes = {})
       @config = Config.new
