3d1679693323e3d2e69c356ea4cf4d2835b15094
Steven Hoffman <git@fustrate.com>
2021-08-02 13:49:17 -0700

    max muncy copypasta
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- game_chat_bot/embeds/home_run.rb
+++ game_chat_bot/embeds/home_run.rb
@@ -7,11 +7,17 @@ module GameChatBot
       MINIMUM_SPEED = 110
       BORING_LAUNCH_ANGLE = 18..42
 
+      COPYPASTA = <<~COPYPASTA
+        I get physically angry watching Max Muncy hit. This man just doesn't chase. You can almost hear him sneering
+        "that's 0.16cm outside" as he takes a ball 1. Two-strike counts don't faze him. Then he'll whip out a
+        hellacious dong on the 9th pitch. He's suffocating. He's Max Muncy.
+      COPYPASTA
+
       def to_h
         {
           title: "#{team_emoji} #{@play.dig('result', 'event')} (#{count})",
           description: description,
-          fields: hit_data_fields.append({ name: 'Pitch', value: pitch_type, inline: true }),
+          fields: [*hit_data_fields, { name: 'Pitch', value: pitch_type, inline: true }, copypasta].compact,
           color: color.to_i(16),
           footer: resulting_context
         }
@@ -53,6 +59,12 @@ module GameChatBot
       end
 
       def pitch_type() = "#{event.dig('pitchData', 'startSpeed')} mph #{event.dig('details', 'type', 'description')}"
+
+      def copypasta
+        return unless @play.dig('matchup', 'batter', 'id') == 571970
+
+        { name: 'Max Muncy', value: COPYPASTA }
+      end
     end
   end
 end
