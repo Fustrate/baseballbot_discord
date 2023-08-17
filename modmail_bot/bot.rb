774ffc9c048c01d864fca6aa4292c2a146e3d4b3
Steven Hoffman <git@fustrate.com>
2023-08-17 16:22:30 -0700

    start working on code to check modmail and post to discord

--- modmail_bot/bot.rb
+++ modmail_bot/bot.rb
@@ -48,6 +48,8 @@ module ModmailBot
 
     protected
 
+    def subreddit = (@subreddit ||= @reddit.session.subreddit('baseball'))
+
     def discord_server = (@discord_server ||= server 709901748445249628)
 
     def modmail_channel = (@modmail_channel ||= channel 1141768383797416018)
@@ -63,6 +65,62 @@ module ModmailBot
       @scheduler.join
     end
 
-    def check_modmail; end
+    def check_modmail
+      @modmail ||= @reddit.session.modmail
+
+      with_reddit_account do
+        @modmail.conversations(subreddits: [subreddit], limit: 10, sort: :recent).each { process_modmail(_1) }
+      end
+    end
+
+    def process_modmail(conversation)
+      conversation_last_updated = conversation.last_updated.to_i
+
+      thread_last_updated = redis.hget('discord_threads', conversation.id)
+
+      return post_thread!(conversation) unless thread_last_updated
+
+      update_thread!(conversation, after: thread_last_updated) if conversation_last_updated > thread_last_updated
+    end
+
+    def post_thread!(conversation)
+      thread = modmail_channel.start_thread(name, 7 * 24 * 60, type: :forum)
+
+
+
+      redis.hset('discord_threads', conversation.id, conversation.last_updated.to_i)
+    end
+
+    def update_thread!(conversation, after:)
+      since = Time.at(after)
+
+      thread = modmail_channel
+
+      conversation.messages.each do |message|
+        next if message.date.to_i < since
+
+        post_message_to_thread!(thread, message)
+      end
+
+      redis.hset('discord_threads', conversation.id, conversation.last_updated.to_i)
+    end
+  end
+end
+
+module Discordrb
+  # A Discord channel, including data like the topic
+  class Channel
+    def start_thread(name, auto_archive_duration, message: nil, type: 11)
+      message_id = message&.id || message
+      type = TYPES[type] || type
+
+      data = if message
+              API::Channel.start_thread_with_message(@bot.token, @id, message_id, name, auto_archive_duration)
+            else
+              API::Channel.start_thread_without_message(@bot.token, @id, name, auto_archive_duration, type)
+            end
+
+      Channel.new(JSON.parse(data), @bot, @server)
+    end
   end
 end
