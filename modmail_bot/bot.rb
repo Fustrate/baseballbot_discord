4c2ddcb19fe8570c0040df504e222a8c2387d83e
Steven Hoffman <git@fustrate.com>
2023-08-18 10:49:19 -0700

    skips tags right now

--- modmail_bot/bot.rb
+++ modmail_bot/bot.rb
@@ -91,8 +91,7 @@ module ModmailBot
     def post_thread!(conversation)
       channel = modmail_channel.start_forum_thread(
         "#{conversation.user[:name]}: #{conversation.subject}",
-        { content: conversation_to_discord(conversation) },
-        applied_tags: tags_for(conversation)
+        { content: conversation_to_discord(conversation) }
       )
 
       update_redis!(conversation, channel)
