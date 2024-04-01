5b602955ee603000d851c1286cbd6c0afb270988
Steven Hoffman <fustrate@me.com>
2024-04-01 15:14:48 -0700

    sent a message on ready
    Signed-off-by: Steven Hoffman <fustrate@me.com>

---
+++ baseballbot/events/ready.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module BaseballDiscord
+  module Events
+    module Ready
+      extend Discordrb::EventContainer
+
+      # Send a message to the /r/baseball modlogs channel to show that a restart has occurred.
+      ready do
+        channel(476402294171107359).send_message '', false, {
+          title: 'Restarted',
+          description: 'The bot has been restarted.',
+          color: 'ffffff'.to_i(16)
+        }
+
+        nil
+      end
+    end
+  end
+end
